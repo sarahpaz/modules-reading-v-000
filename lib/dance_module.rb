815c5b1d8696e1d8ad972f79a2509be58c783a94
Sarah Paz <sarah.paz@gmail.com>
2019-02-05 01:19:15 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -8,10 +8,9 @@ module Dance
   end
 
   def pirouette
-    "I'm doing a piroutte"
+    "I'm doing a pirouette"
   end
 
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
-end
